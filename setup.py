bbd32611d67fc7ff2485e8e4f3efb137d595ada8
Nathan Estrada <naestrada19@gmail.com>
2017-03-12 08:21:36 -0700

    Fixed issues with setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ os.environ['TCL_LIBRARY'] = "C:\\Users\\naest_000\\AppData\Local\Programs\\Pytho
 os.environ['TK_LIBRARY'] = "C:\\Users\\naest_000\\AppData\\Local\\Programs\\Python\\Python36\\tcl\\tk8.6"
 
 includes      = []
-include_files = [r"C:\\Users\\naest_000\\AppData\\Local\\Programs\\Python\\Python36\\DLLs\\tcl86t.dll",
-                 r"C:\\Users\\naest_000\\AppData\\Local\\Programs\\Python\\Python36\\DLLs\\tk86t.dll"]
+include_files = ["C:\\Users\\naest_000\\AppData\\Local\\Programs\\Python\\Python36\\DLLs\\tcl86t.dll",
+                 "C:\\Users\\naest_000\\AppData\\Local\\Programs\\Python\\Python36\\DLLs\\tk86t.dll"]
 
 setup(
-    name = "Test",
-    version = "1.0",
-    options = {"build_app": {"includes": includes, "include_files": include_files}},
+    name = "Mod Manager",
+    version = "1.1",
+    options = {"build_exe": {"includes": includes, "include_files": include_files}},
     executables = [Executable("main.py", base=base)]
 )
